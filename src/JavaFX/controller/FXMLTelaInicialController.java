0d09fd81fc811c3cbadb97a6f17a8d94bcc31a21
RuanMiniguite <ruanminiguite@gmail.com>
2021-09-22 14:37:00 -0300

    :art: Tela Principal

--- src/JavaFX/controller/FXMLTelaInicialController.java
+++ src/JavaFX/controller/FXMLTelaInicialController.java
@@ -5,19 +5,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author ruanm
- */
 public class FXMLTelaInicialController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }

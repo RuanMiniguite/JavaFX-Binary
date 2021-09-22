94c7ad11a36254cdbafd50d979c90452ea482ccc
RuanMiniguite <ruanminiguite@gmail.com>
2021-09-22 15:16:26 -0300

    :art: icon

--- src/JavaFX/JavaFXBinary.java
+++ src/JavaFX/JavaFXBinary.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -12,10 +13,14 @@ public class JavaFXBinary extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("View/FXMLTelaInicial.fxml"));
-        
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/javaFX/img/b-512x512.png"));
+        stage.setTitle("Binary");
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
+        stage.setResizable(false);
         stage.show();
     }
 

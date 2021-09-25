5a2c91db439ca2d5661eca79ed6514c42c831934
RuanMiniguite <ruanminiguite@gmail.com>
2021-09-25 19:54:47 -0300

    :package: Abrir o arquivo de palavras

--- src/JavaFX/controller/FXMLTelaInicialController.java
+++ src/JavaFX/controller/FXMLTelaInicialController.java
@@ -1,11 +1,19 @@
 package JavaFX.controller;
 
 import java.awt.Button;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.Initializable;
-
 import javafx.fxml.FXML;
+
 
 public class FXMLTelaInicialController implements Initializable {
     
@@ -15,19 +23,31 @@ public class FXMLTelaInicialController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        SortearPalavra();
+        try {
+            abrirArquivo();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FXMLTelaInicialController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLTelaInicialController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    
-
-    @FXML
-    public void SortearPalavra(){
-       
-    
+    public void abrirArquivo() throws FileNotFoundException, IOException{
+        int i = 0;
+        System.out.println("Test");
         
+        FileReader arq = new FileReader("C:\\Users\\ruanm\\Documents\\Github\\JavaFX-Binary\\src\\JavaFX\\database\\Lista-de-Palavras.txt");
+        BufferedReader lerArq = new BufferedReader(arq);
+        String linha = lerArq.readLine();
+        
+        while(i < 10){
+            System.out.printf("%s\n", linha);
+            linha = lerArq.readLine();
+                    
+            i++;
+        }
+        
+        arq.close();
     }
-
-    
-    
     
 }

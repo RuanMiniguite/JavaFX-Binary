9b88200ebbbb0f42a97164a0c4f9b18444f23d07
RuanMiniguite <ruanminiguite@gmail.com>
2021-09-25 19:13:03 -0300

    :art: Botão OK

--- src/JavaFX/controller/FXMLTelaInicialController.java
+++ src/JavaFX/controller/FXMLTelaInicialController.java
@@ -1,15 +1,33 @@
-
 package JavaFX.controller;
 
+import java.awt.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-public class FXMLTelaInicialController implements Initializable {
+import javafx.fxml.FXML;
 
+public class FXMLTelaInicialController implements Initializable {
+    
+    @FXML
+    private Button ok;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        SortearPalavra();
+    }
+    
+    
+
+    @FXML
+    public void SortearPalavra(){
+       
+    
         
-    }    
+    }
+
+    
+    
     
 }

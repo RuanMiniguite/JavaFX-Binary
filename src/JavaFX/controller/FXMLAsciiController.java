e7453331af124e5562401e5c1f32a2ce004993cb
RuanMiniguite <ruanminiguite@gmail.com>
2021-09-27 13:50:19 -0300

    :bulb: Descrição

--- src/JavaFX/controller/FXMLAsciiController.java
+++ src/JavaFX/controller/FXMLAsciiController.java
@@ -1,27 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package JavaFX.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author ruanm
- */
 public class FXMLAsciiController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    
     }    
     
 }
